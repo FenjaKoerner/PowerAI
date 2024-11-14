a20f0d2d4abff2000eb23569cd81dbd9b687fcc0
FenjaKoerner <59176180+FenjaKoerner@users.noreply.github.com>
2024-11-14 17:40:49 +0100

    changed

--- StartPowerAI.bat
+++ StartPowerAI.bat
@@ -1,4 +1,4 @@
 CD C:\Users\Kids\iCloudDrive\Documents\GitHub\PowerAI
 CALL .venv/Scripts/activate.bat
 START jupyter notebook
-"C:\Program Files\MySQL\MySQL Server 9.0\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 9.0\my.ini" -uroot -p34921083
+"C:\Program Files\MySQL\MySQL Server 9.0\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 9.0\my.ini" -uroot -p1234
