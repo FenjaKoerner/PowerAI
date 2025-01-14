8b31d46dc45e70f9e58ad7f84c7a643393409de2
FenjaKoerner <59176180+FenjaKoerner@users.noreply.github.com>
2025-01-14 22:04:52 +0100

    Delete StartPowerAI.bat

--- StartPowerAI.bat
+++
@@ -1,5 +0,0 @@
-CD C:\Users\Kids\iCloudDrive\Documents\GitHub\PowerAI
-CALL .venv/Scripts/activate.bat
-START
-START jupyter notebook
-"C:\Program Files\MySQL\MySQL Server 9.0\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 9.0\my.ini" -uroot -p1234
